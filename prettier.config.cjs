0b77b49aa591d169ab9245ac0b63df60b308ba58
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-05-13 01:36:36 +0800

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+const config = {
+  plugins: [
+    require.resolve('@trivago/prettier-plugin-sort-imports'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+  importOrder: [
+    '^react',
+    '^next',
+    '<THIRD_PARTY_MODULES>',
+    '^@w+\\w',
+    '^@\\w',
+    '^./',
+  ],
+  importOrderSeparation: true,
+};
+
+module.exports = config;
